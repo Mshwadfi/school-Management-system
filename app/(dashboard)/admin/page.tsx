cc316655512a253d4c974d20058be27332099f9e
Mshwadfi <mohamedalshwadfy24@gmail.com>
2024-10-07 18:41:15 +0300

    responsiv navbar

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -3,8 +3,13 @@
 
 const AdminPage = () => {
     return (
-      <div className="">
-        admin
+      <div className="p-2 flex flex-col gap-3 md:flex-row">
+        <div className="w-full md:w-2/3">
+            left
+        </div>
+        <div className="w-full md:w-1/3">
+            right
+        </div>
       </div>
     );
   };
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -17,10 +18,9 @@ export default function DashboardLayout({
             </Link>
             <Menu />
          </div>
-         <div className="bg-slate-400 w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-            right
-            {/* navbar */}
+         <div className="bg-[#F7F8FA] w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] flex flex-col overflow-scroll">
             
+            <Navbar />
             {children}
          </div>
         </div>
