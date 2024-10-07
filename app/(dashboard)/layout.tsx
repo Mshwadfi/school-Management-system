98fdf39efe4d45476160a741ab71f4e291d44c70
Mshwadfi <mohamedalshwadfy24@gmail.com>
2024-10-07 18:22:42 +0300

    responsive menu

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -6,12 +10,17 @@ export default function DashboardLayout({
     return (
         <div className="flex h-screen"
         >
-         <div className="bg-slate-100 w-2/6 md:w-1/6 lg:w-2/6">
-            side Menu
+         <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+            <Link href={'/'} className="flex items-center justify-center lg:justify-start gap-2">
+                <Image src={'/logo.png'} width={32} height={32} alt="logo"/>
+                <span className="hidden md:block">School</span>
+            </Link>
+            <Menu />
          </div>
-         <div className="bg-slate-400 w-4/6 md:w-5/6 lg:w-4/6">
+         <div className="bg-slate-400 w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
             right
             {/* navbar */}
+            
             {children}
          </div>
         </div>
